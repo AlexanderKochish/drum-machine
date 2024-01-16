131489fc16dddb8fe2bb2ffa373d375f0df58660
AlexanderKochish <shvepsolek@gmail.com>
2024-01-16 17:52:17 +0000

    clean main js

--- main.js
+++ main.js
@@ -5,10 +5,6 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Hello Vite!</h1>
   </div>
 `
 
