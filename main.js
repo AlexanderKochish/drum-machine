9923df000eb52b69329f86d79391129857e731c4
AlexanderKochish <shvepsolek@gmail.com>
2024-01-16 17:51:05 +0000

    first one

--- main.js
+++ main.js
@@ -9,9 +9,6 @@ document.querySelector('#app').innerHTML = `
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
